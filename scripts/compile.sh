44cda39909153edc3cde1e01ceb925bf87006965
greencrow <slonik256@yandex.ru>
2021-05-14 18:38:10 +0300

    version 0.0.4

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,8 +5,8 @@ apt-get install python-tqdm -y;
 sudo -u pi ls -la ;
 sudo -u pi sh -c cd ~/ros_catkin_ws/src/ws_service_pkg_1 && echo 'in ws_service_pkg_1';
 sudo -u pi git reset --hard 48aa30ccc74008d7a4aba8f8855184cbecd1c5e8;
-sudo -u pi sh -c source /opt/ros/melodic/setup.bash;
+#sudo -u pi sh -c source /opt/ros/melodic/setup.bash;
 ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/melodic --pkg=ws_serv_A;
-sudo -u pi sh -c cd ~/ros_catkin_ws/ && roslaunch ws_service_pkg_1 start_configure_A.launch;
+sudo -u pi sh -c source /opt/ros/melodic/setup.bash && sudo -u pi roslaunch ws_service_pkg_1 start_configure_A.launch;
 
 
