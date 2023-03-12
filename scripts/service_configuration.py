2fa4d54f073202f1bf11c17345b3ff82bd83b212
ThisIsCateee <127566681+ThisIsCateee@users.noreply.github.com>
2023-03-12 16:20:41 +0900

    Update service_configuration.py

--- scripts/service_configuration.py
+++ scripts/service_configuration.py
@@ -4,8 +4,9 @@ import time
 import tqdm
 # import rospy
 
-configuration_number = 5341.4
+configuration_number = 5015
 version = "1.0.0"
+range: 7500
 
 print("Service package 1: ver. {}".format(version))
 time.sleep(0.8)
