c9541608729cece3345d45123eec370b2546de77
chek77 <chek1502@mail.ru>
2023-04-20 11:10:07 +0300

    Update

--- scripts/service_configuration.py
+++ scripts/service_configuration.py
@@ -4,14 +4,14 @@ import time
 import tqdm
 # import rospy
 
-configuration_number = 5341.4
+configuration_number = 5015
 version = "1.0.0"
 
 print("Service package 1: ver. {}".format(version))
 time.sleep(0.8)
 print("Service package 1: start configuration ")
 time.sleep(0.5)
-for i in tqdm.tqdm(range(10000), ascii=True, desc="System check"):
+for i in tqdm.tqdm(range(7500), ascii=True, desc="System check"):
     time.sleep(0.001)
 
 print("Service package 1: successfully configured!")
